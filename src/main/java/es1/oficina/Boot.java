80c2a88d88b2607f2909d8c5ef01a05938440b93
André Victor <andrevctr12@gmail.com>
2018-05-30 19:23:47 -0300

    root adicionado

--- src/main/java/es1/oficina/Boot.java
+++ src/main/java/es1/oficina/Boot.java
@@ -2,6 +2,8 @@ package es1.oficina;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class Boot
@@ -10,5 +12,13 @@ public class Boot
    public static void main(String[] args)
    {
       SpringApplication.run(Boot.class, args);
+
+   }
+
+   @GetMapping("/")
+   @ResponseBody
+   public String home()
+   {
+      return "home";
    }
 }
